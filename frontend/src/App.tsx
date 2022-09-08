3d75ad3f0e9703748c9c9eede2ea039a5911158f
dan1618 <daniel.malek10@googlemail.com>
2022-09-08 16:07:52 +0200

    graph library added with use case

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Workspace from './workspace/Workspace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '1300px',width: '1300px'}} className="App">
+      <Workspace></Workspace>
     </div>
   );
 }
