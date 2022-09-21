696328c23b54b59e99e7312f6b45864ebd46db70
dan1618 <daniel.malek10@googlemail.com>
2022-09-21 13:15:32 +0200

    css grid

--- frontend/src/component/controls-panel/ControlsPanel.tsx
+++ frontend/src/component/controls-panel/ControlsPanel.tsx
@@ -1,16 +1,25 @@
-import {createUseStyles} from 'react-jss'
+import {createUseStyles} from 'react-jss';
+import { Button, Heading } from '@chakra-ui/react';
 const useStyles = createUseStyles({
-   something: {
-     background: 'lightgrey',
+  container: {
+    textAlign: 'center'
+   },
+   startButton: {
+    margin: '24px 0px',
+    background: '#6f45d8',
+    '&:hover': {
+      background: '#5836af'
+    }
    }
  })
  function ControlsPanel({ data }:any) {
    const classes = useStyles()
  
    return (
-     <div className={classes.something}>
-         bbb
-     </div>
+    <div className={classes.container}>
+    <Heading as='h4' size='md'>Controls</Heading>
+    <Button className={classes.startButton} colorScheme='blue'>START FLOW</Button>
+ </div>
    );
  }
  export default ControlsPanel
