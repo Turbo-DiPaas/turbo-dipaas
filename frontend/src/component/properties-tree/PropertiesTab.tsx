0785cb0e462fda34fdb9fae63152c24419e2c77f
dan1618 <daniel.malek10@googlemail.com>
2022-09-23 10:46:56 +0200

    style improvement

--- frontend/src/component/properties-tree/PropertiesTab.tsx
+++ frontend/src/component/properties-tree/PropertiesTab.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@chakra-ui/react';
 import React, {Component} from 'react';
 import './Tree.css'
 
@@ -14,16 +15,22 @@ class Tree extends React.Component<any, any>  {
         const margin = 40
         const tree = data.map((node, key) => {
             if(node.data === null) {
-              return <div className="Tree" style={{marginLeft: String((level + 1) * margin) + "px"}} key={key}>{node.name}</div>
+            //   return <div className="Tree" style={{marginLeft: String((level + 1) * margin) + "px"}} key={key}>{node.name}</div>
+              return <Button className="Tree" style={{marginLeft: String((level + 1) * margin) + "px"}} colorScheme='blue' variant='ghost' size='sm'>
+               {node.name}
+               </Button>
             }
             return <Tree key={key} name={node.name} data={node.data} level={level+1} />
         })
         const sign = this.state.show ? " \u2212 " : " + "
         return (
             <>
-                <div className="Tree" style={{marginLeft: String(level * margin) + "px"}} onClick={this.toggleView}>
+                <Button className="Tree" style={{marginLeft: String(level * margin) + "px"}}  colorScheme='blue' variant='ghost' size='sm' onClick={this.toggleView}>
+                     {sign}{name}
+                </Button>
+                {/* <div className="Tree" style={{marginLeft: String(level * margin) + "px"}} onClick={this.toggleView}>
                     <span className="Sign">{sign}</span>{name}
-                </div>
+                </div> */}
                 {this.state.show && tree}
             </>
         )
